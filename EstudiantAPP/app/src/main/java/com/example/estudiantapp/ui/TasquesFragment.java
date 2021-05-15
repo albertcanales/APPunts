bf045653d88d7436d6b5e41e082a0e25c7699579
Albert Canales Ros <albertcanalesros@gmail.com>
2021-05-15 03:08:51 +0200

    Implemented new task activity

--- EstudiantAPP/app/src/main/java/com/example/estudiantapp/ui/TasquesFragment.java
+++ EstudiantAPP/app/src/main/java/com/example/estudiantapp/ui/TasquesFragment.java
@@ -1,6 +1,8 @@
 package com.example.estudiantapp.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +10,8 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.example.estudiantapp.NewTaskActivity;
+import com.example.estudiantapp.R;
 import com.example.estudiantapp.databinding.FragmentTasquesBinding;
 
 public class TasquesFragment extends Fragment {
@@ -19,6 +23,14 @@ public class TasquesFragment extends Fragment {
 
         binding = FragmentTasquesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        root.findViewById(R.id.newtask_bt).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.println(Log.ASSERT, "JAJA", "lol");
+                startActivity(new Intent(getActivity(), NewTaskActivity.class));
+            }
+        });
 
         return root;
     }
