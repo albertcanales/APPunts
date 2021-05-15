2a1dd49ee88fdb20f06ebf848154fda6b24ac0f5
AlferesChronos <aleixtorresca@gmail.com>
2021-05-15 19:27:07 +0200

    Taskes acabades

--- EstudiantAPP/app/src/main/java/com/example/estudiantapp/MainActivity.java
+++ EstudiantAPP/app/src/main/java/com/example/estudiantapp/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.estudiantapp;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 
 import com.example.estudiantapp.db.Task;
+import com.example.estudiantapp.db.TxtHandler;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
@@ -19,37 +21,50 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.estudiantapp.databinding.ActivityMainBinding;
 import com.google.gson.Gson;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
+import static com.example.estudiantapp.db.TxtHandler.fromTaskListToString;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    private Context context;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("TXT", "myPrint");
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        context = this;
 
-
-        Log.d("TXT", "myPrint");
         Task task1 = new Task("Entregable 4", "CD", new Date(2021, 12, 3));
         Task task2 = new Task("Entregable 5", "CD", new Date(2021, 12, 3));
         Task task3 = new Task("Entregable 6", "CD", new Date(2021, 12, 3));
         Task task4 = new Task("Entregable 7", "CD", new Date(2021, 12, 3));
         Task task5 = new Task("Entregable 8", "CD", new Date(2021, 12, 3));
-        Log.d("TXT", "myPrint");
+
         List<Task> taskList = new ArrayList<Task>(){{add(task1); add(task2); add(task3); add(task4); add(task5);}};
 
-        Gson gson = new Gson();
-        String json = gson.toJson(taskList, Task.class);
+        // Gson gson = new Gson();
 
-        Log.d("JSON", json);
+        // String txt = fromTaskListToString(taskList);
+
+        /*
+        try {
+            TxtHandler.prova(context, taskList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        */
+
+
 
         setSupportActionBar(binding.appBarMain.toolbar);
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
@@ -85,17 +100,5 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
-    }
-
-    public static String fromTaskToString(Task task) {
-        Date date = task.getDate();
-        String s = String.format("%1;%2;%3;%4;%5",
-                task.getNom(), task.getAssignatura(), date.getYear(), date.getMonth(), date.getDate());
-        return s;
-    }
-
-    public static Task getTaskFromString(String txtString) {
-        String[] parts = txtString.split(";");
-        return new Task(parts[0], parts[1], new Date(Integer.parseInt(parts[2]), Integer.parseInt(parts[3]), Integer.parseInt(parts[4])));
     }
 }
